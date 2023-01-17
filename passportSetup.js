e5e8b72f4129c137f3c361dae01170fab0b512bc
Aman Wadhwaniya <amanwadhwaniya159@gmail.com>
2023-01-17 23:28:15 +0530

    Changed!

--- passportSetup.js
+++ passportSetup.js
@@ -16,7 +16,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://blog-api-3tml.onrender.com/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, callback) {
       // User.findOrCreate({ googleId: profile.id }, function (err, user) {
